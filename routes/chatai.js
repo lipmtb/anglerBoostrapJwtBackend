2badd7633ae870a384d2457a928693097e0ffacc
lipmtb <littpm@163.com>
2023-07-09 11:53:12 +0800

    增加多轮对话ai接口

--- routes/chatai.js
+++ routes/chatai.js
@@ -45,4 +45,52 @@ router.post("/gptai", bodyParser.json(), (req, res) => {
     });
 })
 
+
+// openaiapi
+router.post("/longTurnAi", bodyParser.json(), (req, res) => {
+    const content = req.body.content;
+    const apiKey = req.body.apiKey || "";
+    if (!apiKey) {
+        res.send({
+            errCode: 1,
+            message: "请输入apiKey"
+        })
+        return;
+    }
+    const requestConfig = {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + apiKey
+        },
+    };
+    axios.post(OPENAI_URL, {
+        model: "text-davinci-003",
+        prompt: content,
+        temperature: 1,
+        max_tokens: 500,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0.2,
+        stop: ["Human", "AI"],
+    }, requestConfig).then(response => {
+        console.log("chatgpt响应成功:", response.data);
+        const responseBuffer = JSON.stringify({
+            errCode: 0,
+            choices: response.data.choices,
+            message: "成功"
+        });
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Content-Length', responseBuffer.length);
+        res.write(responseBuffer);
+        res.end();
+    }).catch(error => {
+        console.error("请求失败", error);
+        res.send({
+            choices: null,
+            errCode: 1,
+            message: "失败"
+        })
+    });
+})
+
 module.exports = router;
